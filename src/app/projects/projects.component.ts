d441e552e9e897ba44a5973c24e30eab499f14a2
Juanblink <juan.manuel.chavez.peralta@uabc.edu.mx>
2025-06-22 22:30:29 -0700

    Added new images blur

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,7 +14,7 @@ export class ProjectsComponent {
       title: 'System for Human Resources',
       description: 'Comprehensive Human Resources management system designed to efficiently handle all employee-related aspects. Includes modules for employee records management, uniform allocation and tracking, vacation requests and approvals, attendance monitoring, performance evaluations, and compliance management. Streamlines internal processes, enhances communication, and ensures complete organization of the company’s human capital..',
       images: [
-        '',
+        'https://i.postimg.cc/HW86KKgS/Menu.jpg',
       ],
     },
     {
@@ -22,7 +22,7 @@ export class ProjectsComponent {
       title: 'System for Overtime Management and Budget Planning',
       description: 'Robust management system for tracking employee overtime hours, creating and managing budgets, and generating detailed visualizations and reports. Features include accurate overtime registration, budget planning tools, interactive charts for data analysis, and comprehensive reporting to support informed decision-making and operational efficiency.',
  images: [
-      '', // URL directa a imagen
+      'https://i.postimg.cc/tCh2Rx7W/OVT.jpg', // URL directa a imagen
     ],
     },
     {
@@ -30,7 +30,7 @@ export class ProjectsComponent {
       title: 'Proyect for Ideas',
       description: 'This project is a lightweight internal system designed for employees of a large company to submit and share their ideas. It includes features such as user roles, a points-based system, and proposal tracking.',
       images: [
-        '',
+        'https://i.postimg.cc/m2xmGwZT/Ideando.jpg',
       ],
     },
     {
